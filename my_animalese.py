12b8e39caec569ba72735e6fa9d668f06b7aa90d
小安 <andy5343927@gmail.com>
2020-07-27 21:53:18 +0800

    都還是沒有變

--- my_animalese.py
+++ my_animalese.py
@@ -47,8 +47,13 @@ def main():
         num = index + 1
         if num < 10:
             num = "0" + str(num)
-        sounds[ltr] = "./sounds/" + argument.pitch + \
-            "/sound" + str(num) + ".wav"
+        sounds[ltr] = (
+            "./sounds/" +
+            argument.pitch +
+            "/sound" +
+            str(num) +
+            ".wav"
+        )
 
     if argument.rnd_factor is None:
         if argument.pitch == "med":
@@ -61,50 +66,51 @@ def main():
     infiles = []
 
     for i, char in enumerate(stringy):
-        try:
-            if char == "s" and stringy[i+1] == "h":  # test for "sh" sound
-                infiles.append(sounds["sh"])
-                continue
-            elif char == "t" and stringy[i+1] == "h":  # test for "th" sound
-                infiles.append(sounds["th"])
-                continue
-            # test if previous letter was "s" or "s" and current letter is "h"
-            elif char == "h" and (stringy[i-1] == "s" or stringy[i-1] == "t"):
-                continue
-            elif char == "," or char == "?":
-                infiles.append(sounds["."])
-                continue
-            elif char == stringy[i-1]:  # skip repeat letters
-                continue
-        except:
+        if char == "s" and stringy[i+1] == "h":  # test for "sh" sound
+            infiles.append(sounds["sh"])
+        elif char == "t" and stringy[i+1] == "h":  # test for "th" sound
+            infiles.append(sounds["th"])
+        # test if previous letter was "s" or "s" and current letter is "h"
+        elif char == "h" and (stringy[i-1] == "s" or stringy[i-1] == "t"):
+            pass
+        elif char == "," or char == "?":
+            infiles.append(sounds["."])
+        elif char == stringy[i-1]:  # skip repeat letters
             pass
         # skip characters that are not letters or periods.
-        if not char.isalpha() and char != ".":
-            continue
-        infiles.append(sounds[char])
+        elif char.isalpha() or char == ".":
+            infiles.append(sounds[char])
+        # skip characters that are not letters or periods.
+        else:
+            pass
 
     combined_sounds = None
 
-    print(len(infiles))
+    print("Length of text is", len(infiles))
     for index, sound in enumerate(infiles):
         tempsound = AudioSegment.from_wav(sound)
         if stringy[len(stringy)-1] == "?":
             if index >= len(infiles)*.8:
                 # shift the pitch up by half an octave
-                # (speed will increase proportionally)
+                # speed will increase proportionally
                 octaves = random.random() * rnd_factor + (index-index*.8) * .1 + 2.1
             else:
                 octaves = random.random() * rnd_factor + 2.0
         else:
             # shift the pitch up by half an octave
-            # (speed will increase proportionally)
+            # speed will increase proportionally
             octaves = random.random() * rnd_factor + 2.3
         new_sample_rate = int(tempsound.frame_rate * (2.0 ** octaves))
-        new_sound = tempsound._spawn(tempsound.raw_data, overrides={
-            "frame_rate": new_sample_rate})
-        new_sound = new_sound.set_frame_rate(
-            argument.uniform_sample_rate)  # set uniform sample rate
-        combined_sounds = new_sound if combined_sounds is None else combined_sounds + new_sound
+        new_sound = tempsound._spawn(
+            tempsound.raw_data,
+            overrides={"frame_rate": new_sample_rate}
+        )
+        # set uniform sample rate
+        new_sound = new_sound.set_frame_rate(argument.uniform_sample_rate)
+        if combined_sounds:
+            combined_sounds = combined_sounds + new_sound
+        else:
+            combined_sounds = new_sound
 
     combined_sounds.export("./sound.wav", format="wav")
 
